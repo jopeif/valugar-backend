89891631862ec2fed9309b25ceace931411194a0
jopeif <joao.pedro.lima06@aluno.ifce.edu.br>
2025-10-03 16:22:57 -0300

    fix: reorder middleware setup for express application

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,9 @@ import AuthRouter from './infra/web/routes/AuthRoutes';
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+
 
 app.use(express.json());
-
-app.use('/auth/', AuthRouter);
 
 app.use(cors({
   origin: ["http://localhost:5173", "http://localhost:3000", "https://valugar.vercel.app", "https://admin.valugar.vercel.app"], 
@@ -19,6 +17,10 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
+app.use('/auth/', AuthRouter);
+
+
 
 
 setupSwagger(app);

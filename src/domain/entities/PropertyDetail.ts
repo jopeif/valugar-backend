295c333b28324f49826b89013343fa24930fb798
jopeif <joao.pedro.lima06@aluno.ifce.edu.br>
2025-10-05 17:32:39 -0300

    feat: add Address and PropertyDetails entities with build and assemble methods

---
+++ src/domain/entities/PropertyDetail.ts
@@ -0,0 +1,35 @@
+export type PropertyDetailsProps = {
+    id: string,
+    area: string,
+    bedrooms: number,
+    bathrooms: number,
+    listing: string,
+}
+
+export class PropertyDetails {
+    private constructor(private props: PropertyDetailsProps){}
+
+    static build(
+        area: string,
+        bedrooms: number,
+        bathrooms: number,
+        listing: string,
+    ): PropertyDetails {
+        const props: PropertyDetailsProps = {
+            id: crypto.randomUUID(),
+            area,
+            bedrooms,
+            bathrooms,
+            listing,
+        }
+        return new PropertyDetails(props)
+    }
+
+    static assemble(props: PropertyDetailsProps){
+        return new PropertyDetails(props)
+    }
+
+    getProps(){
+        return this.props
+    }
+}
